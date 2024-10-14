ee671f16b373981fd53ebaecda6a6ac2fd8801f6
unknown <adj2424@gmail.com>
2024-10-14 01:24:45 -0400

    GOLDGOLDGOLD

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,70 +1,63 @@
-import { useEffect } from 'react';
+import { memo, useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 
-export const Loading = () => {
+gsap.registerPlugin();
+
+// issue with only using ref is that use state in parent is causing rerender breaking things
+// using memo to prevent rerender from parent but also means component is not being rerendered so we use a new ref on the div to change the dom value
+export const Loading = memo(() => {
+	const countRef = useRef(0);
+	const countDisplayRef = useRef(null) as any;
+	const distanceRef = useRef(0);
+	const timeline = useRef(gsap.timeline()).current;
+
 	/**
 	 * Recursive loading screen
 	 */
 	useEffect(() => {
-		load(0);
-	});
+		const endPosition = countDisplayRef.current.getBoundingClientRect().right;
+
+		distanceRef.current = (window.innerWidth - endPosition) * 0.88;
+
+		// timeline.to(countDisplayRef.current, {
+		// 	x: distanceRef.current,
+		// 	duration: 10
+		// });
+
+		load();
+	}, []);
 
 	// use timeline seek maybe with percent?
-	function load(count: number) {
+	const load = () => {
 		// finished loading and play enter animation
-		if (count >= 100) {
-			// const header = document.getElementById('header-name')!;
-			// const font = window.getComputedStyle(header).fontSize;
-			// const { top, left } = header.getBoundingClientRect();
-			// gsap.to('#load-name', {
-			// 	fontSize: font,
-			// 	top: top,
-			// 	left: left,
-			// 	duration: 1.5,
-			// 	ease: 'power2.out',
-			// 	delay: 0.5
-			// });
-
-			// // wait for .5 seconds
-			// setTimeout(() => {
-			// 	let loadingPercent = new SplitType('#loading', { types: 'chars' });
-			// 	gsap
-			// 		.timeline()
-			// 		.to(loadingPercent!.chars, {
-			// 			yPercent: -200,
-			// 			stagger: 0.15,
-			// 			duration: 0.5,
-			// 			delay: 0.5
-			// 		})
-			// 		.to('.loader-container', {
-			// 			opacity: 0,
-			// 			duration: 0.5,
-			// 			delay: 0.1,
-			// 			ease: 'power2.out'
-			// 		})
-			// 		.add(() => {
-			// 			document.querySelector('.loader-container')?.remove();
-			// 		});
-			// }, 500);
+		if (countRef.current >= 100) {
 			return;
 		}
 		const d = Math.random();
-		count += d > 0.95 ? Math.ceil(Math.sqrt(d) * 25) : 1;
-		if (count > 100) {
-			count = 100;
+		countRef.current += d > 0.95 ? Math.ceil(Math.sqrt(d) * 25) : 1;
+
+		if (countRef.current > 100) {
+			countRef.current = 100;
 		}
-		// const loading = document.getElementById('loading')!;
-		// loading.innerHTML = `${count}%`;
-		// let scalePercent = (count * 82) / 100;
-		// document.getElementById('loading')!.style.right = 82 - scalePercent + '%';
-		console.log(count);
-		setTimeout(load, 40, count);
-	}
+		console.log(countRef.current, d, distanceRef.current);
+		countDisplayRef.current.textContent = `${countRef.current}%`;
+		gsap.to(countDisplayRef.current, {
+			x: (distanceRef.current * countRef.current) / 100,
+			duration: 1
+		});
+		setTimeout(load, 200);
+	};
 
 	return (
 		<>
-			<div className="flex flex-col justify-between items-center absolute w-full h-full pointer-events-none z-[1] ">
-				loading page
+			<div className="flex items-center justify-center absolute w-full h-full pointer-events-none z-[1] bg-white opacity-0">
+				<div id="loading" className="text-[8rem]">
+					✨LOADING✨
+				</div>
+				<div ref={countDisplayRef} className="absolute bottom-0 left-[1rem] text-[8rem]">
+					0%
+				</div>
 			</div>
 		</>
 	);
-};
+});
